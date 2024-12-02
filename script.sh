4dfc97abf19bc0b610da6343b5514aeb11a0b92a
izumisu <izumisu@tutamail.com>
2024-12-02 13:00:34 +0800

    ls

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Convert markdown to html
-pandoc index.md -o index.html
+pandoc *.md -o *.html
 
 #Upload file to server directory /web
 lftp ftp://3mmmts729i222:SS89CjM1@103.132.234.237 << EOF
-put ~/blog/index.html
+put ~/blog/{index, 202411}.html
 
 bye
 EOF
