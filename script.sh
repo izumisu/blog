ae76e11cf50c67ab04809fd421b7139f78a5f7bc
izumisu <izumisu@tutamail.com>
2024-12-02 13:02:17 +0800

    Try adding 202411

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 #Convert markdown to html
-pandoc *.md -o *.html
+pandoc index.md -o index.html
+pandoc 202411.md -o 202411.html
 
 #Upload file to server directory /web
 lftp ftp://3mmmts729i222:SS89CjM1@103.132.234.237 << EOF
-put ~/blog/{index, 202411}.html
+put ~/blog/index.html
+put ~/blog/202411.html
 
 bye
 EOF
